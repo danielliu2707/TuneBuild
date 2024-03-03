7f619e2f615ef1e5d90750900c3f2ed3a2512287
Daniel Liu <danielliu2707@gmail.com>
2024-03-03 20:46:38 +1100

    Modify default port number for application opening.
    This commit modifies the port number used when opening the application on the web after the user runs app.py.
    This is necessary to allow the user to specify the correct callback URI in the Spotify Developer Application.

--- app.py
+++ app.py
@@ -165,7 +165,7 @@ def create_playlist():
     return render_template('create-playlist.html')
 
 if __name__ == "__main__":
-    port = 5000 + random.randint(0, 999)
+    port = 5000
     url = "http://127.0.0.1:{0}".format(port)
     threading.Timer(1.25, lambda: webbrowser.open(url) ).start()
     app.run(port=port, debug=False)
